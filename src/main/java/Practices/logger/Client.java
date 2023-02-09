b88c42f00d4ee745a6adaf63185f91c372012e29
Amy312 <ambarrojasm@gmail.com>
2023-02-09 02:11:27 -0400

    Initial commit

---
+++ src/main/java/Practices/logger/Client.java
@@ -0,0 +1,26 @@
+package Practices.logger;
+
+import java.io.IOException;
+
+public class Client {
+    private String name;
+
+    public Client(String name) {
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void enterClient() throws IOException {
+        String register = "Ingresó el/la cliente " + this.name + " a la tienda";
+        Logger.getInstance().addRegister(register);
+    }
+
+    public void leaveClient() throws IOException {
+        String register = "Se retira el/la cliente " + this.name + " de la tienda";
+        Logger.getInstance().addRegister(register);
+    }
+
+}
